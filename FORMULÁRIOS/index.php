cedea86ffdbf8016ee06fc0dc461844b5e5673e9
Clayce Kellen <claycekellen21@gmail.com>
2022-03-28 22:56:15 -0300

    usando formulários em php

---
+++ FORMULÁRIOS/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</title>
+</head>
+<body>
+
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+    <form action="script.php" method="post"> <!-- action: arquivo para enviar os dados-->
+        <p>Seu nome: <input type="text" name= "nome"></p>
+        <p>Sua idade: <input type="text" name= "idade"></p>
+        <p><input type="submit" value="Enviar dados"></p>
+
+    </form>
+
+</body>
+</html>

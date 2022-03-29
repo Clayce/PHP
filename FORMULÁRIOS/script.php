f1171ffc23a12ade9fe5d207ec1ea05254cfa6e4
Clayce Kellen <claycekellen21@gmail.com>
2022-03-28 23:31:22 -0300

    trabalhando com formulários

---
+++ FORMULÁRIOS/script.php
@@ -0,0 +1,87 @@
+<?php
+    $categorias= [];
+    $categorias[]= 'infantil';
+    $categorias[]= 'adolescente';
+    $categorias[]= 'adulto';
+
+    $nome = $_POST['nome']; //pega info vinda do formulário
+    $idade = $_POST['idade'];
+
+    if(empty($nome)) // verifica se a var está vazia
+    {
+        echo 'Nome não pode ser vazio!';
+        return;
+    }
+
+    if(strlen($nome) < 3) // strlen: conta  a quantidade de cacactere na var
+    {
+        if($nome == "Jó")
+        {
+            if($idade >= 6 && $idade <= 12 )
+            {
+                for($i = 0; $i < count($categorias); $i++)
+                {
+                if($categorias[$i] == 'infantil') 
+                    echo $nome . ' compete na categoria infantil';//mostra info na tela, se true
+                }
+            }
+
+            elseif($idade >= 13 && $idade<= 18)
+            {
+                for($i = 0; $i < count($categorias); $i++)
+                {
+                if($categorias[$i] == 'adolescente') 
+                    echo $nome.' compete na categoria adolescente';//mostra info na tela, se true
+                }
+            }
+
+            else
+            {
+                echo $nome . ' compete na categoria adulto';//mostra info na tela, se true
+            }
+
+        }
+        echo "Nome não pode ter menos de 3 letras!";
+        return;
+    }
+
+    if(strlen($nome) > 30) //se nome informado for maior que 30
+    {
+        echo 'O nome é muito extenso!';
+        return;
+    }
+
+    if(!is_numeric($idade)) // !is_numeric: se NÃO FOR númerico
+    {
+        echo 'Valor precisa ser numérico!';
+        return;
+    }
+
+    // var_dump($nome);
+    // var_dump($idade);
+    //return 0;
+    
+    if($idade >= 6 && $idade <= 12 )
+    {
+        for($i = 0; $i < count($categorias); $i++)
+        {
+           if($categorias[$i] == 'infantil') 
+            echo $nome . ' compete na categoria infantil';//mostra info na tela, se true
+        }
+    }
+
+    elseif($idade >= 13 && $idade<= 18)
+    {
+        for($i = 0; $i < count($categorias); $i++)
+        {
+           if($categorias[$i] == 'adolescente') 
+            echo $nome.' compete na categoria adolescente';//mostra info na tela, se true
+        }
+    }
+
+    else
+    {
+        echo $nome . ' compete na categoria adulto';//mostra info na tela, se true
+    }
+    
+?>
